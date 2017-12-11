76ee2c78b7d7df760bf97340aac8ab930569263c
dmlsthe1 <dxangobiz@gmail.com>
2017-12-11 15:18:41 -0800

    passing tests cleaned ui

--- src/topspot.jsx
+++ src/topspot.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
 export default props => (
-    <div className="well border border-dark bg-white p-3">
+    <div className="well jumbotron p-3">
         <h4>{props.name}</h4>
         <p>{props.description}</p>
-        <a href={`https://maps.google.com/?q=${props.location[0]},${props.location[1]}`}><button type="button">Link</button></a>
+        <a href={`https://maps.google.com/?q=${props.location[0]},${props.location[1]}`}><button className="btn btn-primary" type="button">Link</button></a>
     </div>
 );
