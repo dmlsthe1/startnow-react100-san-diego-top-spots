ca16d3c7919e9cd4ea6dbb4576b4b61c9bfb5018
dmlsthe1 <dxangobiz@gmail.com>
2018-01-03 11:22:25 -0800

    updated and added features

--- src/app.jsx
+++ src/app.jsx
@@ -6,14 +6,35 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      topspots: []
+      topspots: [],
     }
+    this.handleMouseOut = this.handleMouseOut.bind(this);
+    this.handleMouseOver = this.handleMouseOver.bind(this)
   }
 
   componentWillMount(){
     axios.get("https://origin-top-spots-api.herokuapp.com/api/topspots")
       .then(response => response.data)
       .then(topspots => this.setState({topspots}));
+  }
+
+  componentDidMount(){
+    axios.get(`https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=40.6655101,-73.89188969999998&destinations=1234,5678&key=AIzaSyBHQCbrnxTUiD8R53sCZ-SzR1M5OACf1-w`)
+      .then(response => response.data)
+  }
+
+  handleMouseOver(e){
+    let id = e.target.id;
+    this.setState({
+      [id]: "block"
+    })
+  }
+
+  handleMouseOut(e){
+    let id = e.target.id;
+    this.setState({
+      [id]: "none"
+    })
   }
 
   render() {
@@ -26,10 +47,13 @@ class App extends Component {
           {this.state.topspots.map(topspot =>
             <TopSpot 
               key={topspot.id}
+              index={topspot.id}
               name={topspot.name}
               description={topspot.description}
               location={topspot.location}
-              handleClick={this.handleClick}
+              id={this.state[topspot.id]}
+              handleMouseOver={this.handleMouseOver}
+              handleMouseOut={this.handleMouseOut}
             />)}
       </div>
     );
--- src/topspot.jsx
+++ src/topspot.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 
 export default props => (
-    <div className="well jumbotron p-3">
+    <div onMouseEnter={props.handleMouseOver} onMouseLeave={props.handleMouseOut} id={props.index} className="well jumbotron p-3">
         <h4>{props.name}</h4>
-        <p>{props.description}</p>
-        <a href={`https://maps.google.com/?q=${props.location[0]},${props.location[1]}`}><button className="btn btn-primary" type="button">Link</button></a>
+        <p style={{display: props.id ? props.id: "none"}}>{props.description}</p>
+        <div className="container">
+            <div className="row">
+                <a href={`https://maps.google.com/?q=${props.location[0]},${props.location[1]}`}>
+                    <button className="btn btn-primary col" type="button">
+                        <strong>Google Maps Link </strong>(Distance: {props.index})
+                    </button>
+                </a>
+                <p className="col">{}</p>
+            </div>
+        </div>
     </div>
 );
